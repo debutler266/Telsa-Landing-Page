017f0b50c9d75fa1c9c2cba6b9982fd86ee88ff3
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-11-04 16:45:02 -0400

    added the structure & styled buttons for webpage. Used a new/modern ES6 method for Button markup

--- tesla-landing-page-app/src/components/Button.js
+++ tesla-landing-page-app/src/components/Button.js
@@ -1,7 +1,13 @@
-const Button = () => {
-  return (
-    <div className='button'>
+import React from 'react';
+import './Button.css';
 
+
+const Button = ({ imp, text, link }) => {
+  return (
+    <div className={`button ${imp == 'secondary' ? 'button__white' : ''}`}>
+    <a href={link}>
+    {text}
+    </a>
     </div>
   );
 }
